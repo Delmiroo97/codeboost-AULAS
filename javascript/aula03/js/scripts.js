9f6557790b6d8f663b279ad30eeb3a0e257bd951
Fernando Delmiro <119796795+Delmiroo97@users.noreply.github.com>
2025-05-14 10:34:21 -0300

    Aprendendo sobre os tipos de dados.

---
+++ javascript/aula03/js/scripts.js
@@ -0,0 +1,5 @@
+let nome = 'Fernando'; // string
+let ano = 2025; // Number
+let carroVendido = true; // Bolean 
+
+console.log(`Nome: ${nome} em que ano estou: ${ano}`); // concatenação entre a viariavel nome + ano 
